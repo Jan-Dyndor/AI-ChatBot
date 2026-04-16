b5d3fdfba306334759b92e569a3050f4ed54a8e8
Jan Dyndor <jandyndor@gmail.com>
2026-04-16 20:35:13 +0200

    llama3:8b without context in terminal

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from ai-chatbot!")
+from ollama import chat
 
-
-if __name__ == "__main__":
-    main()
+while True:
+    i = input("Enter prompt ").strip()
+    if i == "exit":
+        break
+    response = chat(model="llama3:8b", messages=[{"role": "user", "content": i}])
+    print("AI: ", response.message.content)

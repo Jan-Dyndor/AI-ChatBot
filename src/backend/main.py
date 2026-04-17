afa2daa5e8b7eef23b1db0263f2315626967824e
Jan Dyndor <jandyndor@gmail.com>
2026-04-17 11:21:38 +0200

    adding history of conversation to model

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,21 @@
 from ollama import chat
 
-
 if __name__ == "__main__":
+    history = []
     while True:
-        i = input("Enter prompt ").strip()
-        if i == "exit":
+
+        user_input = input("Enter prompt: ").strip()
+        if user_input == "exit":
             break
-        response = chat(model="llama3:8b", messages=[{"role": "user", "content": i}])
-        print("AI: ", response.message.content)
+
+        message = {
+            "role": "user",
+            "content": user_input,
+        }
+
+        history.append(message)
+        response = chat(model="llama3:8b", messages=history)
+        ai_response_content = response.message.content
+        ai_message = {"role": "assistant", "content": ai_response_content}
+        print("AI: ", ai_response_content)
+        history.append(ai_message)

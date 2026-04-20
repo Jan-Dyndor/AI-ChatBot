b76e86424bab6cba2ba4203b49717cecad38c05f
Jan Dyndor <jandyndor@gmail.com>
2026-04-20 18:58:26 +0200

    added env file and pydantic settings to read file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Installed python code as package wit
 uv pip install -e . from root of the project
+Notes:
+- Do not use this app only as API! FastAPI docs are unable to process StreamingResponses - use curl or streamlit frontend! 
+
+1. Add API_URL in .env file of project repo
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from backend.api.router.v1 import router
+from contextlib import asynccontextmanager
+from backend.configuration.settings import get_settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Before app starts
+    get_settings()  # read .env file
+    yield
+    # After shutdown
+
 
 app = FastAPI()
 app.include_router(router)

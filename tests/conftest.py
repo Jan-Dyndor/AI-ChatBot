ef7ffd3eb984a0e3ff26a901f71462d02113c36f
Jan Dyndor <jandyndor@gmail.com>
2026-04-24 16:13:24 +0200

    endpoint test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
-from backend.api.schemas.pydantic_schemas import ChatMessage, UserInput
-from fastapi.testclient import TestClient
+import time
+
 import pytest
+from fastapi.testclient import TestClient
+
+from backend.api.schemas.pydantic_schemas import ChatMessage, UserInput
 from backend.main import app
 
 
@@ -10,7 +13,35 @@ def happy_test_user_input_short():
         input="What are you?",
         chat_history=[ChatMessage(role="assistant", content="What are you")],
         model="llama3:8b",
-    )
+    ).model_dump()
+
+
+@pytest.fixture
+def happy_model_stream_response():
+    """Function mock the behaviour or Streaming Response from AI Ollama model"""
+
+    def streaming_generator(chat_history):
+        for word in [
+            "I",
+            "am",
+            "powerfull",
+            "AI!",
+            "I",
+            "am",
+            "here",
+            "to",
+            "destroy",
+            "you!",
+        ]:
+            yield word + " "
+            time.sleep(0.01)
+
+    return streaming_generator
+
+
+@pytest.fixture
+def model_stream_response():
+    return "I am powerfull AI! I am here to destroy you!".strip()
 
 
 @pytest.fixture
--- tests/test_api_chat.py
+++ tests/test_api_chat.py
@@ -1,1 +1,24 @@
+from unittest.mock import Mock, patch
 
+from backend.chat_bot.client import ChatBot
+
+
+@patch.object(ChatBot, "stream_response")
+def test_chat_streaming(
+    chatbot_instance,
+    client,
+    happy_test_user_input_short,
+    happy_model_stream_response,
+    model_stream_response,
+):
+
+    chatbot_instance.side_effect = happy_model_stream_response
+
+    response = client.post("v1/chat", json=happy_test_user_input_short)
+
+    chunks = []
+    for chunk in response.iter_text():
+        chunks.append(chunk)
+
+    full_response_txt = "".join(chunks)
+    assert full_response_txt.strip() == model_stream_response

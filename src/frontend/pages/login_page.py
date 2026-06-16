5abb19f23dd2f6f1d395ef41852b0fa0703f9b0f
Jan Dyndor <dyndorjan@gmail.com>
2026-06-16 22:17:18 +0200

    Added logging and request ID to frontend logs. Frontend is responsible for requestID creation(#32)
    Co-authored-by: Jan Dyndor <jandyndor@gmail.com>

--- src/frontend/pages/login_page.py
+++ src/frontend/pages/login_page.py
@@ -1,9 +1,17 @@
+from uuid import uuid4
+
 import requests
 import streamlit as st
-from backend.configuration.settings import get_settings
 from loguru import logger
 
+from backend.configuration.settings import get_settings
+
 settings = get_settings()
+REQUEST_ID: str = str(
+    uuid4()
+)  # Create Request ID, Streamlit will run this ptyhon script each time so fresh ID will occure
+
+
 st.set_page_config(
     page_title="Login",
     page_icon="🔐",
@@ -17,7 +25,6 @@ st.caption("Log in to use the Conversational AI App")
 if "access_token" in st.session_state and st.session_state.access_token:
     st.switch_page("pages/chatbot_page.py")
 
-
 with st.form("login_form"):
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
@@ -28,42 +35,51 @@ with st.form("login_form"):
         if not email or not password:
             st.error("Please provide email and password.")
         else:
-            try:
-                response = requests.post(
-                    settings.api_token_url,
-                    json={
-                        "email": email,
-                        "password": password,
-                    },
-                    timeout=10,
-                )
-                data = response.json()
-
-                if response.status_code == 401:
-                    logger.warning(
-                        f"User {email} does not exists or porvided invalid password"
+            with logger.contextualize(request_id=REQUEST_ID):
+                try:
+                    response = requests.post(
+                        settings.api_token_url,
+                        json={
+                            "email": email,
+                            "password": password,
+                        },
+                        headers={"Request-ID": f"{REQUEST_ID}"},
+                        timeout=10,
                     )
-                    st.error("Invalid email or password.")
-                    st.stop()
-                elif response.status_code == 422:
-                    logger.warning(
-                        f"User {email} entered wrongly formated input data - {data}"
-                    )
-                    st.error(f"Invalid data provided: {data}")
-                    st.stop()
+                    logger.debug(f"User {email} login request")
+                    data = response.json()
 
-                response.raise_for_status()
+                    backend_request_id = response.headers.get("Request-ID")
+                    if backend_request_id != REQUEST_ID:
+                        logger.error(
+                            f"Request IDs does not match. Check Logs. Frontend generated {REQUEST_ID} and backed gave back {backend_request_id}"
+                        )
 
-                st.session_state.access_token = data["access_token"]
-                st.session_state.token_type = data.get("token_type", "bearer")
+                    if response.status_code == 401:
+                        logger.warning(
+                            f"User {email} does not exists or porvided invalid password"
+                        )
+                        st.error("Invalid email or password.")
+                        st.stop()
+                    elif response.status_code == 422:
+                        logger.warning(
+                            f"User {email} entered wrongly formated input data - {data}"
+                        )
+                        st.error(f"Invalid data provided: {data}")
+                        st.stop()
 
-                logger.debug(f"User {email} logged successfully")
-                st.success("Logged in successfully.")
-                st.switch_page("pages/chatbot_page.py")
+                    response.raise_for_status()
 
-            except requests.RequestException:
-                logger.exception(f"Exception occured while logging User {email}")
-                st.error("Backend is unavailable. Check if FastAPI is running.")
+                    st.session_state.access_token = data["access_token"]
+                    st.session_state.token_type = data.get("token_type", "bearer")
+
+                    logger.debug(f"User {email} logged successfully")
+                    st.success("Logged in successfully.")
+                    st.switch_page("pages/chatbot_page.py")
+
+                except requests.RequestException:
+                    logger.exception(f"Exception occured while logging User {email}")
+                    st.error("Backend is unavailable. Check if FastAPI is running.")
 
 
 st.markdown("---")
--- src/frontend/pages/register_page.py
+++ src/frontend/pages/register_page.py
@@ -1,9 +1,15 @@
+from uuid import uuid4
+
 import requests
 import streamlit as st
 from loguru import logger
+
 from backend.configuration.settings import get_settings
 
 settings = get_settings()
+REQUEST_ID: str = str(
+    uuid4()
+)  # Create Request ID, Streamlit will run this ptyhon script each time so fresh ID will occure
 
 st.set_page_config(
     page_title="Register",
@@ -32,38 +38,50 @@ with st.form("register_form"):
         elif password != password_repeat:
             st.error("Passwords do not match.")
         else:
-            try:
-                response = requests.post(
-                    settings.api_create_user_url,
-                    json={
-                        "email": email,
-                        "password": password,
-                    },
-                    timeout=10,
-                )
-
-                if response.status_code == 409:
-                    st.error("User with this email already exists.")
-                    logger.warning(
-                        f"Register User Response: {response.status_code} User with this email {email} already exists"
+            with logger.contextualize(request_id=REQUEST_ID):
+                try:
+                    response = requests.post(
+                        settings.api_create_user_url,
+                        json={
+                            "email": email,
+                            "password": password,
+                        },
+                        headers={
+                            "Request-ID": f"{REQUEST_ID}",
+                        },
+                        timeout=10,
                     )
-                    st.stop()
-                elif response.status_code == 422:
-                    error = response.json()
-                    st.error(f"Invalid data provided {error}")
-                    logger.warning(
-                        f"Register User  {email} response with wrongly formated input data. Status code: {response.status_code} . Error: {error}"
+                    logger.debug(f"User {email} registration request")
+                    backend_request_id = response.headers.get("Request-ID")
+                    if backend_request_id != REQUEST_ID:
+                        logger.error(
+                            f"Request IDs does not match. Check Logs. Frontend generated {REQUEST_ID} and backed gave back {backend_request_id}"
+                        )
+
+                    if response.status_code == 409:
+                        st.error("User with this email already exists.")
+                        logger.warning(
+                            f"Register User Response: {response.status_code} User with this email {email} already exists"
+                        )
+                        st.stop()
+                    elif response.status_code == 422:
+                        error = response.json()
+                        st.error(f"Invalid data provided {error}")
+                        logger.warning(
+                            f"Register User  {email} response with wrongly formated input data. Status code: {response.status_code} . Error: {error}"
+                        )
+                        st.stop()
+
+                    response.raise_for_status()
+
+                    st.success("Account created successfully. You can now log in.")
+                    logger.debug(f"Successfull User registration {email}")
+
+                except requests.RequestException:
+                    st.error("Backend is unavailable. Check if FastAPI is running.")
+                    logger.exception(
+                        f"Exception occured while registering User {email}"
                     )
-                    st.stop()
-
-                response.raise_for_status()
-
-                st.success("Account created successfully. You can now log in.")
-                logger.debug(f"Successfull User registration {email}")
-
-            except requests.RequestException:
-                st.error("Backend is unavailable. Check if FastAPI is running.")
-                logger.exception(f"Exception occured while registering User {email}")
 
 st.markdown("---")
 
